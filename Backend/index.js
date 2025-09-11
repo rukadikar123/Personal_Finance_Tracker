99a9bfde729480367d421b8e316904f3f23164ef
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-09-11 21:15:05 +0530

    feat: integrate Tailwind CSS and set up routing - Created a new Navbar component.

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,10 @@ dBConnect()
 
 // Middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
